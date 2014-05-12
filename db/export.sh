3eae25874aed7b718a44978919bddc36c43a9bda
dougbtv <douglaskippsmith@gmail.com>
2014-05-12 18:23:45 -0400

    [data] updates to data and export script fixed

--- db/export.sh
+++ db/export.sh
@@ -2,7 +2,7 @@
 mongoexport --db smcman --collection chat --out chat.json
 mongoexport --db smcman --collection notes --out notes.json 
 mongoexport --db smcman --collection admins --out admins.json 
-mongoexport --db smcman --collection counters --file counters.json
-mongoexport --db smcman --collection uploads --file uploads.json
+mongoexport --db smcman --collection counters --out counters.json
+mongoexport --db smcman --collection uploads --out uploads.json
 
 
