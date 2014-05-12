e86924e10630dbdc66dcc98b5543034352ffee64
dougbtv <douglaskippsmith@gmail.com>
2014-05-12 16:59:41 -0400

    [idiocy][docs] cleaning up data import/export scripts

--- db/import.sh
+++ db/import.sh
@@ -2,9 +2,9 @@
 echo "Just clearing out chat...."
 mongo --eval "use smcman; db.chat.remove();"
 
-mongoexport --db smcman --collection chat --out chat.json
-mongoexport --db smcman --collection notes --out notes.json 
-mongoexport --db smcman --collection admins --out admins.json 
-mongoexport --db smcman --collection counters --file counters.json
-mongoexport --db smcman --collection uploads --file uploads.json
+mongoimport --db smcman --collection chat --file chat.json
+mongoimport --db smcman --collection notes --file notes.json
+mongoimport --db smcman --collection admins --file admins.json
+mongoimport --db smcman --collection counters --file counters.json
+mongoimport --db smcman --collection uploads --file uploads.json
 
